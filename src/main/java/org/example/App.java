c30351061a588b11b4b0b6e48c676ff9e483bdd9
Halyna Tesliuk <halyna_tesliuk@epam.com>
2023-04-11 00:09:13 +0100

    added in "App" class implementation CommandLineRunner

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,28 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import lombok.RequiredArgsConstructor;
+import org.example.service.DataProcessedService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@RequiredArgsConstructor
+@SpringBootApplication
+public class App implements CommandLineRunner {
+   private final DataProcessedService service;
+
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(App.class);
+        application.run(args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        String fileName = "src/main/resources/input.txt";
+        try {
+            service.parseFile(fileName);
+        } catch (Exception ex) {
+
+        }
     }
 }
