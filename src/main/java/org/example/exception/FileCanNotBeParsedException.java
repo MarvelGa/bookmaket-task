4d5f283a8c7fc7c10562b07b49abf9899f41f51d
Halyna Tesliuk <halyna_tesliuk@epam.com>
2023-04-11 17:36:11 +0100

    edited custom exception

--- src/main/java/org/example/exception/FileCanNotBeParsedException.java
+++ src/main/java/org/example/exception/FileCanNotBeParsedException.java
@@ -1,6 +1,8 @@
 package org.example.exception;
 
 public class FileCanNotBeParsedException extends RuntimeException{
+    public FileCanNotBeParsedException() {
+    }
 
     public FileCanNotBeParsedException(String message) {
         super(message);
