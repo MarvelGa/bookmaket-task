b8d85a227156807a49a30a0c45f7c5f7f4b8d398
Halyna Tesliuk <halyna_tesliuk@epam.com>
2023-04-12 15:53:41 +0100

    removed the unnecessary constructor in "FileProperlyReadException" class

--- src/main/java/org/example/exception/FileProperlyReadException.java
+++ src/main/java/org/example/exception/FileProperlyReadException.java
@@ -1,8 +1,6 @@
 package org.example.exception;
 
 public class FileProperlyReadException extends RuntimeException{
-    public FileProperlyReadException() {
-    }
 
     public FileProperlyReadException(String message) {
         super(message);
