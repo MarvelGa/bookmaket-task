8abce1949ed55cf40ba8727ffca508044441710c
Halyna Tesliuk <halyna_tesliuk@epam.com>
2023-04-12 15:52:59 +0100

    removed the unnecessary constructor in "BookMarket" model class

--- src/main/java/org/example/model/BookMarket.java
+++ src/main/java/org/example/model/BookMarket.java
@@ -2,16 +2,16 @@ package org.example.model;
 
 import lombok.Data;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 @Data
 public class BookMarket {
     private List<Detail> bid;
-    private List<Detail>  ask;
+    private List<Detail> ask;
 
     public BookMarket() {
-        this.bid = new ArrayList<>();
-        this.ask = new ArrayList<>();
+        this.bid = new LinkedList<>();
+        this.ask = new LinkedList<>();
     }
 }
