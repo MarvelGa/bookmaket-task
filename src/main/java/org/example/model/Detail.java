910662e645935e7a3ce3725e82a63c09f042d16b
Halyna Tesliuk <halyna_tesliuk@epam.com>
2023-04-18 22:12:17 +0100

    added equals and hashcode

--- src/main/java/org/example/model/Detail.java
+++ src/main/java/org/example/model/Detail.java
@@ -1,6 +1,8 @@
 package org.example.model;
 
 
+import java.util.Objects;
+
 public class Detail {
     private Integer price;
     private Integer size;
@@ -24,5 +26,18 @@ public class Detail {
 
     public void setSize(Integer size) {
         this.size = size;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Detail detail = (Detail) o;
+        return Objects.equals(price, detail.price) && Objects.equals(size, detail.size);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(price, size);
     }
 }
